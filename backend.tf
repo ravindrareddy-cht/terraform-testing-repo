7a307a483694bb28ca94f3d721880021d08a071c
Ravin Guntaka <ravindrareddy@vmware.com>
2022-12-01 22:20:38 +0000

    testing

--- backend.tf
+++ backend.tf
@@ -1,19 +1,15 @@
 terraform {
+
   required_version = ">= 0.13"
 
   backend "s3" {
-    # bucket owned by cht env repo
-    bucket         = "cht-terraform-ng-stg"
-    key            = "cht-terraform-ng-stg/services/cht-svc-data-export/terraform.tfstate"
-    dynamodb_table = "cht-terraform-ng-stg"
+    # bucket is used for atlantis
+    bucket         = "cht-terraform-mgmt"
+    key            = "cht-terraform-mgmt/dummy_test/terraform.tfstate"
     region         = "us-east-1"
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-}
-
-resource "null_resource" example {
-
 }
